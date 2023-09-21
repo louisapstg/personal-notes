c721c5f014f67bf2888265d62f39d8ee756c6620
Louis <louisapstg@gmail.com>
2023-09-21 22:37:34 +0700

    pembuatan file import komponen

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+import Header from "./header";
+import Form from "./Form";
+import Card from "./Card";
+import Button from "./Button";
+import Navbar from "./Navbar";
+
+export { Header, Form, Card, Button, Navbar };
